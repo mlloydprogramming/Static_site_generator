4aa21d4a189c6861f09c53d0f0ee8677e529f490
quitlloydering <mlloydprogramming@gmail.com>
2025-02-20 13:08:25 -0500

    Align with boot.dev solution

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_single(self):
@@ -26,6 +26,14 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_special_characters(self):
         node = HTMLNode(tag="button", props={"aria-label": 'Click "here"', "data-value": "some'value"})
         self.assertEqual(node.props_to_html(), ' aria-label="Click \"here\"" data-value="some\'value"')
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
