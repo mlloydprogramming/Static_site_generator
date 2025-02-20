b2c6b583603f956ffab7b1405c4a8f562b29e80c
quitlloydering <mlloydprogramming@gmail.com>
2025-02-20 11:10:53 -0500

    CH2L1 TextNode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
+main()
